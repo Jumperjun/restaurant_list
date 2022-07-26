3762b0b4574215bbb063c77fe8b01c094af87871
Jumperjun <meat80507@outlook.com>
2022-07-26 16:56:51 +0800

    finish

---
+++ README.md
@@ -0,0 +1,41 @@
+#restaurant-list
+![Index page about Restaurant List](./public/image/list_screenshot.jpg)
+
+##介紹
+1.瀏覽我的餐廳清單
+2.點擊餐廳查看餐廳詳細資訊
+3.搜尋特定餐廳
+
+##開始使用
+1. 請先確認有安裝 node.js 與 npm
+2. 將專案 clone 到本地
+3. 在本地開啟之後，透過終端機進入資料夾，輸入：
+
+   ```bash
+   npm install
+   ```
+
+4. 安裝完畢後，繼續輸入：
+
+   ```bash
+   npm run start
+   ```
+
+5. 若看見此行訊息則代表順利運行，打開瀏覽器進入到以下網址
+
+   ```bash
+   Listening on http://localhost:3000
+   ```
+
+6. 若欲暫停使用
+
+   ```bash
+   ctrl + c
+   ```
+
+##開發工具
+Node.js 10.15.0
+Express 4.17.1
+Express-Handlebars 5.3.3
+Bootstrap 4.3.1
+Font-awesome 5.8.1
---
+++ app.js
@@ -0,0 +1,40 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+const restaurantList=require('./restaurant.json')
+
+// require handlebars in the project
+const exphbs=require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+// routes setting首頁
+app.get('/',(req,res)=>{
+  res.render('index', { restaurant: restaurantList.results })
+})
+
+//search頁面
+app.get('/search', (req, res) => {
+  const querySearch = req.query.keyword.toLowerCase()
+  const restaurants = restaurantList.results.filter(function (item) {
+    return item.name.toLowerCase().includes(querySearch) || item.category.toLowerCase().includes(querySearch)
+  })
+  res.render('index', { restaurant: restaurants, keyword: req.query.keyword })
+})
+
+//show頁面
+app.get('/restaurants/:id',(req,res)=>{
+  const showRestaurant =restaurantList.results.find(function(item){
+    return item.id.toString()===req.params.id
+  })
+  res.render('show',{restaurantInfo:showRestaurant})
+})
+
+// start and listen on the Express server
+app.listen(port,()=>{
+  console.log(`express is listen on http://localhost:${port}`)
+})
