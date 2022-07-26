fd0f357b8a001c22c0a795180c6af4d935276ae4
Jumperjun <meat80507@outlook.com>
2022-07-26 22:06:27 +0800

    js standard update

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const restaurantList=require('./restaurant.json')
+const restaurantList = require('./restaurant.json')
 
 // require handlebars in the project
-const exphbs=require('express-handlebars')
+const exphbs = require('express-handlebars')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -13,7 +13,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 // routes setting首頁
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.render('index', { restaurant: restaurantList.results })
 })
 
@@ -27,14 +27,14 @@ app.get('/search', (req, res) => {
 })
 
 //show頁面
-app.get('/restaurants/:id',(req,res)=>{
-  const showRestaurant =restaurantList.results.find(function(item){
-    return item.id.toString()===req.params.id
+app.get('/restaurants/:id', (req, res) => {
+  const showRestaurant = restaurantList.results.find(function (item) {
+    return item.id.toString() === req.params.id
   })
-  res.render('show',{restaurantInfo:showRestaurant})
+  res.render('show', { restaurantInfo: showRestaurant })
 })
 
 // start and listen on the Express server
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`express is listen on http://localhost:${port}`)
 })
